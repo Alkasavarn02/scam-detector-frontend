b181bac5f4402fa405285cb453facb0f6b8a9d20
Alka Savarn <alkasavarn02@gmail.com>
2024-11-28 11:01:00 +0530

    add back icon

--- src/component/customHeader/index.jsx
+++ src/component/customHeader/index.jsx
@@ -1,13 +1,21 @@
+import styles from "./header.module.css";
+import BackArrow from "../../assests/BackArrow.png"
 import { memo } from 'react';
-import './header.css';
+import { useNavigate } from "react-router-dom";
+import { DASHBOARD } from "../../constants/routes"
 
 const CustomHeader = ({ title, subTitle, classes }) => {
+
+    const navigate = useNavigate();
     return (
         <header className={`text-center green-bg white-text ${classes || ""}`}>
-            <div className='fw-medium ls-2 title'>{title}</div>
+            <div className={styles['back-arrow']} onClick={() => navigate(DASHBOARD)}>
+                <img src={BackArrow} alt="BackArrow" />
+            </div>
+            <div className={`fw-medium ls-2 ${styles['title']}`}>{title}</div>
             {
                 subTitle
-                ? <div className='ls-1 sub-title'>{subTitle}</div>
+                ? <div className={`ls-1 ${styles['sub-title']}`}>{subTitle}</div>
                 : null
             }
         </header>
